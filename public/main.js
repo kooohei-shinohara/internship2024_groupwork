873aa75b19d9d174c56e5a68026272ab0be6cb64
訓練生 04 <TRAINEE04@aeroasahi.co.jp>
2024-09-12 09:41:08 +0900

    不要な行を削除

--- public/main.js
+++ public/main.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
         L.control.layers(baseLayers).addTo(map);
     }
 
-    async function loadSheltersData() {
+    async function loadOverLayMapsData() {
         try {
             const response = await fetch('http://localhost:3002/api/overLayMaps');
             if (!response.ok) {
@@ -59,22 +59,6 @@ document.addEventListener('DOMContentLoaded', () => {
             }).addTo(map);
         } catch (error) {
             console.error('Error fetching shelters data:', error);
-        }
-    }
-
-    function showInfoTable(properties) {
-        const infoTableBody = document.getElementById('infoTableBody');
-        infoTableBody.innerHTML = ''; // 既存の行をクリア
-
-        for (const [key, value] of Object.entries(properties)) {
-            const row = document.createElement('tr');
-            const keyCell = document.createElement('td');
-            keyCell.textContent = key;
-            const valueCell = document.createElement('td');
-            valueCell.textContent = value;
-            row.appendChild(keyCell);
-            row.appendChild(valueCell);
-            infoTableBody.appendChild(row);
         }
     }
 
@@ -115,6 +99,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     layerData();
-    loadSheltersData();
+    loadOverLayMapsData();
     showCurrentLocation();
 });
